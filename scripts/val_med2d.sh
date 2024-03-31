7441e621ae7320dfffd5b1d3cbe1249f5c85dad9
bestswt <K23065445@kcl.ac.uk>
2024-03-31 23:09:59 +0100

    处理文件

--- scripts/val_med2d.sh
+++ scripts/val_med2d.sh
@@ -1,5 +1,5 @@
 python validation.py --seed 2023\
  -vp ./results/vis_sam_med2d_10 \
- -cp ./ckpt/sam_med2d.pth \
+ -cp ./ckpt/sam_med2d_b.pth \
  -tdp /scratch/users/k23065445/data/data/test/ -nc 10 \
  --image_size 256 -mt vit_b --dim 2 --save_name ./results/sam_med2d_10.py --ft2d
