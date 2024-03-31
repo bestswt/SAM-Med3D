b52c4273da226f54c8985b844b5e327404bf037e
bestswt <K23065445@kcl.ac.uk>
2024-03-31 22:41:56 +0100

    处理文件

--- scripts/val_sam.sh
+++ scripts/val_sam.sh
@@ -1,5 +1,5 @@
 python validation.py --seed 2023\
- -vp ./results/vis_sam_vit_b \
+ -vp ./results/vis_sam_vit_b_10 \
  -cp ./ckpt/sam_vit_b.pth \
- -tdp ./data/validation_test1 -nc 10 \
- --image_size 1024 -mt vit_b --dim 2 --save_name ./results/sam_vit_b.py 
+ -tdp /scratch/users/k23065445/data/data/test/ -nc 10 \
+ --image_size 1024 -mt vit_b --dim 2 --save_name ./results/sam_vit_b_10.py
