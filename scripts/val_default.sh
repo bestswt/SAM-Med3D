2f7fdbd955fc6e38d6d6211d72ad5bb29b0efe45
bestswt <K23065445@kcl.ac.uk>
2024-04-09 01:50:25 +0100

    testing

--- scripts/val_default.sh
+++ scripts/val_default.sh
@@ -1,6 +1,6 @@
 python validation.py --seed 2023\
  -vp ./results/vis_sam_med3d \
  -cp ./ckpt/sam_med3d_turbo.pth \
- -tdp /scratch/users/k23065445/data/data/test/ -nc 1 \
+ -tdp /scratch/prj/inf_clinicalllm_msc/wentao_shi/data/word/test -nc 1 \
  --save_name ./results/sam_med3d.py \
  --skip_existing_pred
