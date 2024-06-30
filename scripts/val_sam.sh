8f0e7c9efd4e91d6f37c027801786bedcbf239de
bestswt <K23065445@kcl.ac.uk>
2024-06-30 21:49:18 +0100

    testing

--- scripts/val_sam.sh
+++ scripts/val_sam.sh
@@ -1,5 +1,5 @@
 python validation.py --seed 2023\
- -vp ./results/vis_sam_vit_b \
+ -vp ./results/vis_sam_vit_b_1 \
  -cp ./ckpt/sam_vit_b.pth \
  -tdp /scratch/prj/inf_clinicalllm_msc/wentao_shi/data/data/test/ -nc 1 \
- --image_size 1024 -mt vit_b --dim 2 --save_name ./results/sam_vit_b.py
+ --image_size 1024 -mt vit_b --dim 2 --save_name ./results/sam_vit_b_1.py
