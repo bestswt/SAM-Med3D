b3231f7fc3d134ff98784313cf01c03825d4aaa2
bestswt <K23065445@kcl.ac.uk>
2024-07-11 17:39:02 +0100

    testing

--- infer.sh
+++ infer.sh
@@ -1,8 +1,8 @@
 python inference.py --seed 2024\
  -cp /scratch/users/k23065445/SAM_CT/work_dir/from_turbo117/sam_model_dice_best.pth \
- -tdp /scratch/prj/inf_clinicalllm_msc/wentao_shi/data/data/demo/ -nc 1 \
+ -tdp /scratch/prj/inf_clinicalllm_msc/wentao_shi/data/data/demo/ -nc 10 \
  --output_dir ./results  \
- --task_name infer_ours_117_1 \
+ --task_name infer_ours_117_10 \
 # --skip_existing_pred \
 # --sliding_window \
 # --save_image_and_gt \
