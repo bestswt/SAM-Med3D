18ce08debcb8d303998b1b0089f20402241c0a0f
bestswt <K23065445@kcl.ac.uk>
2024-07-11 18:32:47 +0100

    testing

--- infer.sh
+++ infer.sh
@@ -1,8 +1,8 @@
 python inference.py --seed 2024\
  -cp /scratch/users/k23065445/SAM_CT/work_dir/from_turbo117/sam_model_dice_best.pth \
- -tdp /scratch/prj/inf_clinicalllm_msc/wentao_shi/data/data/demo/ -nc 10 \
+ -tdp /scratch/prj/inf_clinicalllm_msc/wentao_shi/data/data/demo/ -nc 1 \
  --output_dir ./results  \
- --task_name infer_ours_117_10 \
+ --task_name test \
 # --skip_existing_pred \
 # --sliding_window \
 # --save_image_and_gt \
--- inference.py
+++ inference.py
@@ -393,6 +393,7 @@ def save_numpy_to_nifti(in_arr: np.array, out_path, affine):
     # so we need to squeeze and transpose back to HxWxD
     ori_arr = np.transpose(in_arr.squeeze(), (2, 1, 0))
     out = nib.Nifti1Image(ori_arr, affine[0])
+    print(affine[0])
     # print(ori_arr.shape)
     nib.save(out, out_path)
     # out = sitk.GetImageFromArray(ori_arr)
