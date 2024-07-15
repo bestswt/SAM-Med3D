f1d0096167f3e0569fd53d4a4730588085cca1a5
bestswt <K23065445@kcl.ac.uk>
2024-07-15 16:35:47 +0100

    testing

--- infer.sh
+++ infer.sh
@@ -1,14 +1,14 @@
 python inference.py --seed 2024\
  -cp /scratch/users/k23065445/SAM_CT/work_dir/from_turbo117/sam_model_dice_best.pth \
- -tdp /scratch/prj/inf_clinicalllm_msc/wentao_shi/data/data/demo/ -nc 1 \
+ -tdp /scratch/prj/inf_clinicalllm_msc/wentao_shi/data/data/test/ -nc 1 \
  --output_dir ./results  \
- --task_name 117_1 \
+ --task_name 117_1_all \
 
 python inference.py --seed 2024\
  -cp /scratch/users/k23065445/SAM_CT/work_dir/from_turbo117/sam_model_dice_best.pth \
- -tdp /scratch/prj/inf_clinicalllm_msc/wentao_shi/data/data/demo/ -nc 10 \
+ -tdp /scratch/prj/inf_clinicalllm_msc/wentao_shi/data/data/test/ -nc 10 \
  --output_dir ./results  \
- --task_name 117_10 \
+ --task_name 117_10_all \
 
 
 
